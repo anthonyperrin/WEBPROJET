2ee0afb10c3b831f48de35933c5d37595ea8f588
SerahF <anthonyperrinff@gmail.com>
2017-12-07 13:20:17 +0100

    Ignorer

--- inscription.php
+++ inscription.php
@@ -21,7 +21,6 @@
 <body class="bg-primay">
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">La maison du Livre</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
