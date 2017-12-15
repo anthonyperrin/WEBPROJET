14e4550c1beea2282ab1ed1b5511eb1a31125764
anthonyperrin <anthonyperrinff@gmail.com>
2017-12-15 15:02:20 +0100

    All

--- profil.php
+++ profil.php
@@ -70,7 +70,7 @@
         echo "<b>Mail : </b>" . $_SESSION['Mail_Membre'] . "<br><br>";
       ?>
       <form class="" action="index.html" method="GET">
-
+        
       </form>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
